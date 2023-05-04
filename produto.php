ad09834341fd0571b564b1fbbf6d2f2d68ec6eed
NathanMaced <nathanmacedo02@gmail.com>
2023-05-03 21:37:00 -0300

    continuação

---
+++ produto.php
@@ -0,0 +1,17 @@
+<?php
+
+    class Produto{
+        var $codigo;
+        var $Descricao;
+        var $Preco;
+        var $Quantidade;
+
+        function imprimeEtiqueta()
+        {
+            print 'Codigo: ' .$this->Codigo. "<br>";
+            print 'Descrição: ' .$this->Descricao . "<br><br>"
+        }
+    }
+  
+
+?
